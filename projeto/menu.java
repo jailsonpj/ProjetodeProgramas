a0c8ceea677ca698daf04f55fd345793f4f34623
Jefferson Avilar <jlda.snf@uea.edu.br>
2017-05-02 20:21:17 -0400

    menu

---
+++ projeto/menu.java
@@ -0,0 +1,13 @@
+public Class Menu {
+
+public static void main (String[] args) {
+
+		do{
+				String op = JOptionPane.showInputDialog(null,"Informe a operação:\n 
+							[1] cadastrar professor e alunos \n 
+							[2] cadastrar aluno \n 
+							[3] mostrar professor \n 
+							[4] mostrar aluno \n [5] fim\n"); 
+		} While(op != 0);
+	}    
+}
