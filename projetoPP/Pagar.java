adebce471b2b8831b51bec973d98c428c350db09
jailsonpj <jailsoncolares@gmail.com>
2017-05-03 11:35:11 -0400

    comentando classe

--- projetoPP/Pagar.java
+++ projetoPP/Pagar.java
@@ -1,9 +1,9 @@
 package projeto;
-
+//classe que faz a associação entre usuário e dividas 
 public class Pagar {
-	private Usuario user;
-	private Divida divida;
-	private int totalDivida;
+	private Usuario user; //atributo do tipo Usuario
+	private Divida divida; //atributo do tipo Divida
+	private int totalDivida; //variavel para contar o total de dividas a pagar 
 	
 	
 	public int getTotalDivida() {
